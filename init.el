ce59c46c5589c695170af0b06010475faa5a729e
SimonSungm <simonsungm@gmail.com>
2021-05-16 22:42:04 +0800

    remove littering

--- init.el
+++ init.el
@@ -34,3 +34,13 @@
 ;;              (set-terminal-parameter frame 'background-mode mode))
 ;;            (enable-theme 'solarized)))
 (load-theme 'hickey t)
+
+
+
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
